46607cbf3048c762cfb950c651fb6870c0c6af65
HZ <335672336@qq.com>
2023-06-05 21:46:19 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path'; // for static files
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/static' }); // 支持静态资源请求
+  await app.listen(3000);
+}
+bootstrap();
